77ca4c9f8b9d23ab27ec849afd7df16e91f25459
Ben Bradshaw <bcbrad@gmail.com>
2023-11-11 22:28:42 -0700

    center images

--- frontend/src/chat-gpt.ts
+++ frontend/src/chat-gpt.ts
@@ -47,6 +47,10 @@ export class ChatGPT extends LitElement {
       margin: 1rem 30% 1rem auto;
       border-radius: 10px 10px 10px 0;
     }
+    p {
+      display: flex;
+      flex-direction: column;
+    }
     img {
       max-width: 100%;
       max-height: 500px;
@@ -229,55 +233,57 @@ export class ChatGPT extends LitElement {
   get loadingIcon() {
     if (!this.loading) return nothing;
     return html`
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        style="margin: 0 auto; display:block;"
-        width="100px"
-        height="100px"
-        viewBox="0 0 100 100"
-        preserveAspectRatio="xMidYMid">
-        <circle cx="50" cy="50" r="0" fill="none" stroke="#e90c59" stroke-width="2">
-          <animate
-            attributeName="r"
-            repeatCount="indefinite"
-            dur="1s"
-            values="0;40"
-            keyTimes="0;1"
-            keySplines="0 0.2 0.8 1"
-            calcMode="spline"
-            begin="0s"></animate>
-          <animate
-            attributeName="opacity"
-            repeatCount="indefinite"
-            dur="1s"
-            values="1;0"
-            keyTimes="0;1"
-            keySplines="0.2 0 0.8 1"
-            calcMode="spline"
-            begin="0s"></animate>
-        </circle>
-        <circle cx="50" cy="50" r="0" fill="none" stroke="#46dff0" stroke-width="2">
-          <animate
-            attributeName="r"
-            repeatCount="indefinite"
-            dur="1s"
-            values="0;40"
-            keyTimes="0;1"
-            keySplines="0 0.2 0.8 1"
-            calcMode="spline"
-            begin="-0.5s"></animate>
-          <animate
-            attributeName="opacity"
-            repeatCount="indefinite"
-            dur="1s"
-            values="1;0"
-            keyTimes="0;1"
-            keySplines="0.2 0 0.8 1"
-            calcMode="spline"
-            begin="-0.5s"></animate>
-        </circle>
-      </svg>
+      <div style="min-height: 100px; min-width: 100px">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          style="margin: 0 auto; display:block;"
+          width="100px"
+          height="100px"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="xMidYMid">
+          <circle cx="50" cy="50" r="0" fill="none" stroke="#e90c59" stroke-width="2">
+            <animate
+              attributeName="r"
+              repeatCount="indefinite"
+              dur="1s"
+              values="0;40"
+              keyTimes="0;1"
+              keySplines="0 0.2 0.8 1"
+              calcMode="spline"
+              begin="0s"></animate>
+            <animate
+              attributeName="opacity"
+              repeatCount="indefinite"
+              dur="1s"
+              values="1;0"
+              keyTimes="0;1"
+              keySplines="0.2 0 0.8 1"
+              calcMode="spline"
+              begin="0s"></animate>
+          </circle>
+          <circle cx="50" cy="50" r="0" fill="none" stroke="#46dff0" stroke-width="2">
+            <animate
+              attributeName="r"
+              repeatCount="indefinite"
+              dur="1s"
+              values="0;40"
+              keyTimes="0;1"
+              keySplines="0 0.2 0.8 1"
+              calcMode="spline"
+              begin="-0.5s"></animate>
+            <animate
+              attributeName="opacity"
+              repeatCount="indefinite"
+              dur="1s"
+              values="1;0"
+              keyTimes="0;1"
+              keySplines="0.2 0 0.8 1"
+              calcMode="spline"
+              begin="-0.5s"></animate>
+          </circle>
+        </svg>
+      </div>
     `;
   }
 

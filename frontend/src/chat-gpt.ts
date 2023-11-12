527b5ba4514b589684188ed7d03b73502cdae516
Ben Bradshaw <bcbrad@gmail.com>
2023-11-11 22:09:53 -0700

    dall-e working with local filesystem use local dark theme update to display images from markdown and styling

--- frontend/src/chat-gpt.ts
+++ frontend/src/chat-gpt.ts
@@ -1,16 +1,15 @@
-import DOMPurify from 'dompurify';
 import hljs from 'highlight.js';
-import styles from 'highlight.js/styles/github-dark-dimmed.css?inline';
-import { css, html, LitElement, unsafeCSS } from 'lit';
+import { css, html, LitElement, nothing } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 import { marked } from 'marked';
 import { ChatNav } from './chat-nav.js';
+import { githubDarkDimmed } from './github-dark-dimmed.css.js';
 
 @customElement('chat-gpt')
 export class ChatGPT extends LitElement {
   static styles = css`
-    ${unsafeCSS(DOMPurify.sanitize(styles))}
+    ${githubDarkDimmed}
     :host {
       display: flex;
       flex-direction: column;
@@ -39,17 +38,19 @@ export class ChatGPT extends LitElement {
 
     .history.user {
       width: auto;
-      text-align: right;
       margin: 1rem 10% 1rem auto;
       border-radius: 10px 10px 0 10px;
     }
-    .history.user code {
-      text-align: left;
-    }
+
     .history.assistant {
       align-self: flex-start;
       margin: 1rem 30% 1rem auto;
       border-radius: 10px 10px 10px 0;
+    }
+    img {
+      max-width: 100%;
+      max-height: 500px;
+      margin: 0 auto;
     }
     @media (max-width: 1000px) {
       .history {
@@ -96,21 +97,34 @@ export class ChatGPT extends LitElement {
       margin: 0 10px;
     }
   `;
-
+  @state() loading = false;
   @state() history: {
     role: 'user' | 'assistant';
     content: string;
   }[] = [];
+
   async updated() {
     const codeEls = this.shadowRoot.querySelectorAll<HTMLElement>('code:not([data-highlighted])');
     codeEls.forEach((block: HTMLElement) => {
       hljs.highlightElement(block);
     });
   }
+
   async submit() {
+    const engine = document.querySelector<ChatNav>('chat-nav').engine;
+    if (engine !== 'dall-e-3') {
+      await this.runChatReq();
+    }
+    if (engine === 'dall-e-3') {
+      await this.runImageReq();
+    }
+  }
+
+  async runChatReq() {
+    this.loading = true;
+    const engine = document.querySelector<ChatNav>('chat-nav').engine;
     const element = this.shadowRoot?.querySelector('textarea');
     const prompt = element.value;
-    const engine = document.querySelector<ChatNav>('chat-nav').engine;
     const includeContext = document.querySelector<ChatNav>('chat-nav').includeContext;
     element.value = '';
     this.history = [
@@ -155,9 +169,7 @@ export class ChatGPT extends LitElement {
                 }
               ]
             }),
-        prompt: prompt,
-        engine: engine,
-        systemMessage: 'you are a helpful assistant that answers without telling too much backstory'
+        engine: engine
       })
     });
     const reader = resp.body.getReader();
@@ -168,6 +180,7 @@ export class ChatGPT extends LitElement {
       content: ''
     };
     while (true) {
+      this.loading = false;
       const { done, value } = await reader.read();
       if (done) break;
       message = new TextDecoder().decode(value);
@@ -175,12 +188,105 @@ export class ChatGPT extends LitElement {
       this.history = [...this.history];
     }
   }
+  async runImageReq() {
+    this.loading = true;
+    const engine = document.querySelector<ChatNav>('chat-nav').engine;
+    const element = this.shadowRoot?.querySelector('textarea');
+    const prompt = element.value;
+    element.value = '';
+    this.history = [
+      ...this.history,
+      {
+        role: 'user',
+        content: prompt
+      }
+    ];
+    const resp = await fetch('http://localhost:8080/image', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        model: engine,
+        prompt: prompt,
+        n: 1,
+        size: '1024x1024'
+      })
+    });
+    const imgResp = (await resp.json()) as { url: string };
+    const nextIndex = this.history.length;
+    this.history[nextIndex] = {
+      role: 'assistant',
+      // prettier-ignore
+      content: `
+![image](${imgResp.url})
+`
+    };
+    this.history = [...this.history];
+    this.loading = false;
+  }
+
+  get loadingIcon() {
+    if (!this.loading) return nothing;
+    return html`
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        style="margin: 0 auto; display:block;"
+        width="100px"
+        height="100px"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid">
+        <circle cx="50" cy="50" r="0" fill="none" stroke="#e90c59" stroke-width="2">
+          <animate
+            attributeName="r"
+            repeatCount="indefinite"
+            dur="1s"
+            values="0;40"
+            keyTimes="0;1"
+            keySplines="0 0.2 0.8 1"
+            calcMode="spline"
+            begin="0s"></animate>
+          <animate
+            attributeName="opacity"
+            repeatCount="indefinite"
+            dur="1s"
+            values="1;0"
+            keyTimes="0;1"
+            keySplines="0.2 0 0.8 1"
+            calcMode="spline"
+            begin="0s"></animate>
+        </circle>
+        <circle cx="50" cy="50" r="0" fill="none" stroke="#46dff0" stroke-width="2">
+          <animate
+            attributeName="r"
+            repeatCount="indefinite"
+            dur="1s"
+            values="0;40"
+            keyTimes="0;1"
+            keySplines="0 0.2 0.8 1"
+            calcMode="spline"
+            begin="-0.5s"></animate>
+          <animate
+            attributeName="opacity"
+            repeatCount="indefinite"
+            dur="1s"
+            values="1;0"
+            keyTimes="0;1"
+            keySplines="0.2 0 0.8 1"
+            calcMode="spline"
+            begin="-0.5s"></animate>
+        </circle>
+      </svg>
+    `;
+  }
 
   render() {
     return html`
       <div class="history-outer">
+        ${this.loadingIcon}
         ${[...this.history].reverse().map((item) => {
-          const localContent = unsafeHTML(DOMPurify.sanitize(marked.parse(item.content)));
+          const localContent = unsafeHTML(marked.parse(item.content));
 
           return html` <p class="history ${item.role}">${localContent}</p> `;
         })}
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -48,63 +49,48 @@ type Chunk struct {
 	} `json:"choices"`
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
+func handleChatRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("handling chat request")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Content-Type", "text/event-stream")
-	// Read the request body
 	reqBody, err := io.ReadAll(r.Body)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer r.Body.Close()
-
-	// Unmarshal the request body into a ChatPrompt struct
 	var chatPrompt ChatPrompt
 	err = json.Unmarshal(reqBody, &chatPrompt)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
 	err = godotenv.Load()
 	if err != nil {
 		fmt.Println("Error loading .env file")
 		return
 	}
-
 	openAiSk := os.Getenv("OPEN_AI_SK")
-	// Create a ChatRequest
 	fmt.Println("sending request with engine:", chatPrompt.Engine)
 	chatRequest := ChatRequest{
 		Model:    chatPrompt.Engine,
 		Stream:   true,
 		Messages: chatPrompt.Messages,
 	}
-
-	// Marshal the ChatRequest into JSON
 	jsonData, err := json.Marshal(chatRequest)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
-	// Prepare the data for the POST request
 	data := bytes.NewBuffer(jsonData)
-
-	// Create a new request
 	req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", data)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
-	// Set the headers
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", openAiSk))
 	req.Header.Set("Content-Type", "application/json")
-
-	// Send the request
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -116,44 +102,32 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 
 	defer resp.Body.Close()
 
-	// Read the response body line by line (chunk by chunk)
 	reader := bufio.NewReader(resp.Body)
 	var jsonStr string
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
-			// handle error
 			fmt.Println(err)
 			break
 		}
 
-		// Accumulate the chunks
 		jsonStr += strings.TrimPrefix(line, "data: ")
 
-		// Try to unmarshal the accumulated chunks into a Chunk struct
 		var chunk Chunk
 		err = json.Unmarshal([]byte(jsonStr), &chunk)
 		if err != nil {
-			// If there's an error, continue to the next chunk
 			continue
 		}
 
-		// Extract the content from the choices array
 		for _, choice := range chunk.Choices {
 			content := choice.Delta.Content
 
-			// Process the content
-			//fmt.Println(content)
-
-			// Write the content to the response
 			_, err = w.Write([]byte(content))
 			if err != nil {
-				// handle error
 				fmt.Println(err)
 				break
 			}
 
-			// Flush the response writer
 			if f, ok := w.(http.Flusher); ok {
 				f.Flush()
 			} else {
@@ -161,15 +135,113 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 				break
 			}
 		}
-
-		// Reset the accumulated chunks
 		jsonStr = ""
 	}
 }
 
-func main() {
-	http.HandleFunc("/", handleRequest)
+type ImageRequest struct {
+	Model  string `json:"model"`
+	Prompt string `json:"prompt"`
+	N      int    `json:"n"`
+	Size   string `json:"size"`
+}
 
+type ImageResponse struct {
+	Created int `json:"created"`
+	Data    []struct {
+		URL string `json:"url"`
+	} `json:"data"`
+}
+
+func handleImageRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("handling image request")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "application/json")
+	var imgReq ImageRequest
+	if err := json.NewDecoder(r.Body).Decode(&imgReq); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	client := &http.Client{}
+	reqBody, _ := json.Marshal(imgReq)
+	fmt.Println("sending request with body:", string(reqBody))
+	req, err := http.NewRequest("POST", "https://api.openai.com/v1/images/generations", bytes.NewBuffer(reqBody))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
+	openAiSk := os.Getenv("OPEN_AI_SK")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", openAiSk))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+	var res ImageResponse
+	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println("dalle req status code: ", resp.StatusCode)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	imageUrl := res.Data[0].URL
+	resp2, err := http.Get(imageUrl)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp2.Body.Close()
+	t := time.Now()
+	filename := fmt.Sprintf("./frontend/src/assets/dall-e/dall-e_%s.png", t.Format("20060102_150405")) // Create a new file
+	out, err := os.Create(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer out.Close()
+	_, err = io.Copy(out, resp2.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	filename = strings.Replace(filename, "./frontend", "", 1)
+	fileData := map[string]string{
+		"url": filename,
+	}
+	jsonData, err := json.Marshal(fileData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(jsonData)
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+			return
+		}
+		switch r.URL.Path {
+		case "/":
+			handleChatRequest(w, r)
+		case "/image":
+			handleImageRequest(w, r)
+		default:
+			http.NotFound(w, r)
+		}
+	})
 	fmt.Println("Starting server on port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println(err)

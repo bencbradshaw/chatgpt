ae2e96cc20d1f917c5970571ac9924c01066ed19
Ben Bradshaw <bcbrad@gmail.com>
2023-11-17 14:22:18 -0700

    use idb for higher storage limits

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,40 +1,34 @@
+import { IDBPDatabase, openDB } from 'idb';
 import { ChatHistory, ChatHistoryItem } from './types.js';
 
 class Store extends EventTarget {
+  private db: IDBPDatabase;
   private activeHistoryIndex: number;
-  private history: ChatHistoryItem[];
-  private threads: ChatHistory[];
+  private history: ChatHistoryItem[] = [];
+  private threads: ChatHistory[] = [];
+
   constructor() {
     super();
-    this.activeHistoryIndex = localStorage.getItem('activeHistoryIndex')
-      ? Number(localStorage.getItem('activeHistoryIndex'))
-      : 0;
-    this.threads = localStorage.getItem('threads') ? JSON.parse(localStorage.getItem('threads')) : [];
-    this.history = this.threads[this.activeHistoryIndex] || [];
-    this.#emit();
-    console.log('store init', Math.random());
+    this.initDB().then(() => {
+      this.#emit();
+    });
   }
 
-  calculateLocalStorageSize(): number {
-    let totalSize = 0;
-
-    for (let i = 0; i < localStorage.length; i++) {
-      const key = localStorage.key(i);
-      if (key) {
-        const value = localStorage.getItem(key) || '';
-        // Each character is 16 bits (2 bytes) in UTF-16, and size in kilobytes (KB)
-        totalSize += (key.length + value.length) * 2;
+  async initDB() {
+    this.db = await openDB('chatDB', 1, {
+      upgrade(db) {
+        db.createObjectStore('threads', { autoIncrement: true });
+        db.createObjectStore('indices');
       }
-    }
-
-    // Convert from Bytes to Kilobytes (KB)
-    return totalSize / 1024;
+    });
+    this.activeHistoryIndex = (await this.db.get('indices', 'activeHistoryIndex')) || 0;
+    this.threads = (await this.db.get('threads', 1)) || [];
+    this.history = this.threads[this.activeHistoryIndex] || [];
   }
 
-  #writeToLocalStorage() {
-    localStorage.setItem('threads', JSON.stringify(this.threads));
-    localStorage.setItem('activeHistoryIndex', this.activeHistoryIndex.toString());
-    console.log(this.calculateLocalStorageSize().toFixed(2) + ' KB');
+  async #writeToIndexedDB() {
+    await this.db.put('threads', this.threads, 1);
+    await this.db.put('indices', this.activeHistoryIndex, 'activeHistoryIndex');
   }
 
   #emit() {
@@ -46,50 +40,52 @@ class Store extends EventTarget {
     );
   }
 
-  addMessage(message: ChatHistoryItem) {
+  async addMessage(message: ChatHistoryItem) {
     this.history.push(message);
     this.threads[this.activeHistoryIndex] = this.history;
     this.#emit();
-    this.#writeToLocalStorage();
+    this.#writeToIndexedDB();
   }
 
-  addToMessageContent(message: string, index: number) {
+  async addToMessageContent(message: string, index: number) {
     this.history[index].content += message;
-    this.history = [...this.history]; // create a new ref
+    this.history = [...this.history];
     this.threads[this.activeHistoryIndex] = this.history;
-    this.threads = [...this.threads]; // create a new ref
+    this.#writeToIndexedDB();
     this.#emit();
-    this.#writeToLocalStorage();
   }
 
-  selectHistory(index: number) {
-    if (this.threads[index] === undefined) {
+  async selectHistory(index: number) {
+    if (!this.threads[index]) {
       this.threads[index] = [];
     }
     this.history = this.threads[index];
-    this.threads = [...this.threads];
     this.activeHistoryIndex = index;
-    this.#writeToLocalStorage();
+    this.#writeToIndexedDB();
     this.#emit();
   }
-  createNewThread() {
+
+  async createNewThread() {
     this.threads.push([]);
-    this.threads = [...this.threads]; // create a new ref
+    this.#writeToIndexedDB();
     this.selectHistory(this.threads.length - 1);
-    this.#emit();
   }
-  clearOneThread(threadId: number = this.activeHistoryIndex) {
+
+  async clearOneThread(threadId: number = this.activeHistoryIndex) {
     this.threads[threadId] = [];
+    this.#writeToIndexedDB();
     this.#emit();
-    this.#writeToLocalStorage();
   }
-  deleteItem(chatItemId: number, threadId = this.activeHistoryIndex) {}
+
+  async deleteItem(chatItemId: number, threadId = this.activeHistoryIndex) {
+    // Implement deletion logic
+  }
+
   subscribe<T>(key: string, cb: (value: T) => void) {
     const value = this[key] as T;
     cb(value);
     const eventListener = (event: Event) => {
-      const value: T = this[key];
-      cb(value);
+      cb(this[key]);
     };
     this.addEventListener('history-change', eventListener);
     return {
